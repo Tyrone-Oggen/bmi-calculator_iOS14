6f26791ed85ddeafabab5ede98f648cacfe23766
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-06-04 14:33:10 +0200

    slider values liked to IBActions that print out their values on change

--- BMI-Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/Controllers/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let userHeight = String(format: "%.2f", sender.value)
+        
+        print(userHeight)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let userWeight = Int(sender.value)
+        
+        print(userWeight)
+    }
 }
 
