635c255b0b591291a247ad683ca660e76accd466
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-06-24 00:43:58 +0200

    bmi value passed via property on SecondViewController on CalculatePressedButton

--- BMI-Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/Controllers/ViewController.swift
@@ -45,6 +45,7 @@ class ViewController: UIViewController {
         print(bmi)
         
         let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format:"%.1f", bmi)
         
         self.present(secondVC, animated: true, completion: nil)
     }
